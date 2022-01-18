f44f7c235c2c29901aa617e2b8227d5c2abdef67
Chau Tran <ctran@Chaus-MacBook-Pro.local>
2022-01-18 11:52:41 -0600

    feat(bang/async): make connect aware of deps array

--- packages/ngx-bang/async/src/lib/connect.ts
+++ packages/ngx-bang/async/src/lib/connect.ts
@@ -1,22 +1,44 @@
-import { getUnsubscribes, StateProxy } from 'ngx-bang';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import { effect, getUnsubscribes, StateProxy } from 'ngx-bang';
 import type { Observable } from 'rxjs';
 import { isObservable } from 'rxjs';
 
 export function connect<TData extends object, TKey extends keyof TData>(
   stateProxy: StateProxy<TData>,
   stateKey: TKey,
-  connector: Observable<TData[TKey]> | PromiseLike<TData[TKey]>
+  connector:
+    | Observable<TData[TKey]>
+    | [Observable<TData[TKey]>, TKey[]]
+    | PromiseLike<TData[TKey]>
 ): void {
-  const isConnectorObservable = isObservable(connector);
-  if (isConnectorObservable) {
+  const [connectorInput, deps = []]: [
+    Observable<TData[TKey]> | PromiseLike<TData[TKey]>,
+    TKey[]?
+  ] = Array.isArray(connector) ? connector : [connector];
+
+  const isConnectorObservable = isObservable(connectorInput);
+
+  if (!isConnectorObservable) {
+    connectorInput.then((value) => {
+      stateProxy[stateKey] = value;
+    });
+    return;
+  }
+
+  if (deps.length > 0) {
+    effect(stateProxy, deps, () => {
+      const sub = connectorInput.subscribe((value: TData[TKey]) => {
+        stateProxy[stateKey] = value;
+      });
+      return () => {
+        sub.unsubscribe();
+      };
+    });
+  } else {
     getUnsubscribes(stateProxy).add(
-      connector.subscribe((value: TData[TKey]) => {
+      connectorInput.subscribe((value: TData[TKey]) => {
         stateProxy[stateKey] = value;
       })
     );
-  } else {
-    (connector as PromiseLike<TData[TKey]>).then((value) => {
-      stateProxy[stateKey] = value;
-    });
   }
 }
