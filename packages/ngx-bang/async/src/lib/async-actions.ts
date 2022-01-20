a8f62e5595942ae0144ee9b427bd6f2f2c372608
Chau Tran <ctran@Chaus-MacBook-Pro.local>
2022-01-19 21:15:57 -0600

    fix(bang/async): adjust asyncActions

--- packages/ngx-bang/async/src/lib/async-actions.ts
+++ packages/ngx-bang/async/src/lib/async-actions.ts
@@ -14,24 +14,27 @@ type AsyncActionsObservables<TActions extends object> = {
 type AsyncActionsProxy<TActions extends object> = AsyncActions<TActions> &
   AsyncActionsObservables<TActions>;
 
-export function asyncActions<TActions extends object>(
-  actions: TActions
-): AsyncActionsProxy<TActions> {
+export function asyncActions<
+  TActions extends object
+>(): AsyncActionsProxy<TActions> {
   const subCache: Record<string, Subject<any>> = {};
-  return new Proxy(actions, {
-    get: (_, p: string) => {
-      if (p.endsWith('$')) {
-        return (subCache[p] || (subCache[p] = new Subject())).asObservable();
-      }
+  return new Proxy(
+    {},
+    {
+      get: (_, p: string) => {
+        if (p.endsWith('$')) {
+          return (subCache[p] || (subCache[p] = new Subject())).asObservable();
+        }
 
-      return (args: TActions[keyof TActions]) => {
-        const $prop = p + '$';
-        const sub = subCache[$prop] || (subCache[$prop] = new Subject());
-        sub.next(args);
-      };
-    },
-    set: () => {
-      throw new Error('setters are not available on asyncActions');
-    },
-  }) as AsyncActionsProxy<TActions>;
+        return (args: TActions[keyof TActions]) => {
+          const $prop = p + '$';
+          const sub = subCache[$prop] || (subCache[$prop] = new Subject());
+          sub.next(args);
+        };
+      },
+      set: () => {
+        throw new Error('setters are not available on asyncActions');
+      },
+    }
+  ) as AsyncActionsProxy<TActions>;
 }
